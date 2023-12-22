8c5569dd49e1192e6915f4e8af7173e0418b2602
spago <spago@DESKTOP-CSM61C1>
2023-12-22 14:56:42 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+    
+            Random randomGenerator = new Random();
+            int randomNumber = randomGenerator.Next(1, 11);
+
+            Console.WriteLine("Guess the random number 1 - 10");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The random number was {0}", randomNumber);
+
+            if (userGuess < randomNumber)
+            {
+                Console.WriteLine("Your guess was too low");
+            }
+            else if (userGuess > randomNumber)
+            {
+                Console.WriteLine("Your guess was too high");
+            }
+            else
+            {
+                Console.WriteLine("You are correct");
+            }
+        }
+    }
